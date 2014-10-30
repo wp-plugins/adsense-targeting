f00a1314e32b6706ea538acbc1789bf690016b45
tepelstreel <tepelstreel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-30 09:00:13 +0000

    Adsense Targeting 1.5
    git-svn-id: https://plugins.svn.wordpress.org/adsense-targeting/tags/1.4@1016551 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tinymce/langs/de_DE.js
@@ -0,0 +1,6 @@
+// German lang variables for WP3.5
+
+tinyMCE.addI18n({de_de:{
+GoogleIgnoreTags:{	
+desc : 'Google Ignore Tags hinzuf\u00fcgen'
+}}});
---
+++ tinymce/langs/nl_NL.js
@@ -0,0 +1,6 @@
+// Dutch lang variables for WP3.5
+
+tinyMCE.addI18n({nl_nl:{
+GoogleIgnoreTags:{	
+desc : 'Google Ignore Tags toevoegen'
+}}});
